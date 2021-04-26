08f0a499a0323e07e03c7cba8e7c4965df4890bc
Donald Coleman <coleman.drc@gmail.com>
2021-04-26 12:00:48 -0700

    Add loading indicator.

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,5 +1,6 @@
-import {FrameworkConfiguration} from 'aurelia-framework';
+import { PLATFORM } from 'aurelia-pal';
+import { FrameworkConfiguration } from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration): void {
-  //config.globalResources([]);
+  config.globalResources([PLATFORM.moduleName('../loading-indicator')]);
 }
